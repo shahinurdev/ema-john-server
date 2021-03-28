411bb58bf776f0dc726bd0a1fc207fc0edfae3cf
shahinurdev <mdshahinur6@gmail.com>
2021-03-29 00:36:12 +0600

    root adddd

--- index.js
+++ index.js
@@ -10,6 +10,9 @@ const app = express();
 app.use(bodyParser.json())
 app.use(cors());
 const port = 5000;
+app.get('/', (req, res) =>{
+    res.send('it,s working')
+})
 
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
